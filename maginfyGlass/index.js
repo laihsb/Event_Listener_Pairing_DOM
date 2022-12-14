eb876b2b456898846040fb29a6a5d105010e5f89
Lai Saechao-Bazemore <saech@Lais-MacBook-Pro.local>
2022-12-14 18:48:14 -0500

    some spacing and centering

--- maginfyGlass/index.js
+++ maginfyGlass/index.js
@@ -11,9 +11,21 @@ const h1 = document.querySelector('h1');
 h1.style.textAlign = 'center';
 
 const para = document.body.querySelector('p');
+para.style.textAlign = 'center';
+
+//this wraps both the buttons
+const buttonWrapper = document.querySelector('section');
+buttonWrapper.style.textAlign ='center';
+buttonWrapper.style.justifyContent = 'center';
+
+
+
+//this is the button to increase the font size
 let bigger = document.body.querySelector('.bigger');
+bigger.style.fontSize = '20px';
+//this marginRight is the space between the 2 buttons
+bigger.style.marginRight = '20px';
 bigger.addEventListener('click', function(){
-    // count ++;
     if(count < 1){
         count = 1;
     } else if (count > 100){
@@ -27,9 +39,11 @@ bigger.addEventListener('click', function(){
     para.style.fontSize = fontsize;
 
 }); 
+
+//this is the button to decrease font size
 let smaller = document.body.querySelector('.smaller');
+smaller.style.fontSize = '20px';
 smaller.addEventListener('click', function(){
-    // count ++;
     if(count < 1){
         count = 1;
     } else if (count > 100){
@@ -45,10 +59,14 @@ smaller.addEventListener('click', function(){
 }); 
 
 const bigImg = document.querySelector('.bigImg');
-bigImg.style.height = '15px'
+bigImg.style.height = '15px';
+bigImg.style.marginRight = '5px';
 
 const smallImg = document.querySelector('.smallImg');
-smallImg.style.height = '15px'
+smallImg.style.height = '15px';
+smallImg.style.marginRight = '5px';
+
+
 
 
 
