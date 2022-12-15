9125e0a09c2e5fe3c8d6fa045259019a73235619
Lai Saechao-Bazemore <saech@Lais-MacBook-Pro.local>
2022-12-15 09:53:29 -0500

    added visual and btns need to add event

--- craneGame/index.js
+++ craneGame/index.js
@@ -1,1 +1,25 @@
 //Your website should show an image of a crane alongside four buttons. Each button corresponds to a cardinal direction (up, down, left, right), and will move the crane in that direction when clicked.
+
+const wrapper = document.querySelector('body');
+wrapper.style.textAlign = 'center';
+wrapper.style.justifyContent = 'center';
+wrapper.style.alignContent = 'center';
+
+const craneImg = document.querySelector('img');
+craneImg.style.justifyContent = 'center';
+
+const btnL = document.querySelector('.left img');
+btnL.style.height = '15px';
+btnL.style.width = '15px';
+
+const btnU = document.querySelector('.up img');
+btnU.style.height = '15px';
+btnU.style.width = '15px';
+
+const btnR = document.querySelector('.right img');
+btnR.style.height = '15px';
+btnR.style.width = '15px';
+
+const btnD = document.querySelector('.down img');
+btnD.style.height = '15px';
+btnD.style.width = '15px';
